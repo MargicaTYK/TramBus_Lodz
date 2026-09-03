503c019a290967d755c44d8702f0f8e4e8df8d97
MargicaTYK <dn2cr8r88y@privaterelay.appleid.com>
2026-09-04 00:10:14 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1066,7 +1066,8 @@ def index():
     return send_from_directory(app.static_folder, "index.html")
 
 
-if __name__ == "__main__":
-    t = threading.Thread(target=poller_loop, daemon=True)
-    t.start()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
